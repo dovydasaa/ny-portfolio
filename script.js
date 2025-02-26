578d63a143acaf8f68abaf4d4b63666bc9fd78b1
johannes <johannes.snildal@gmail.com>
2025-02-26 18:36:57 +0100

    gorge fix

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 window.onload = function() {
     let counter = 0;
     const maxAlerts = 5;
-    const intervalId = setInterval(function() {
-        if (counter < maxAlerts) {
-            alert("Gorge flod💩");
-            counter++;
-        } else {
-            clearInterval(intervalId);
-        }
-    }, 500); // Alerts every 5 seconds
+    const startDelay = 2000; // 2 seconds delay
+
+    setTimeout(function() {
+        const intervalId = setInterval(function() {
+            if (counter < maxAlerts) {
+                alert("Gorge flod💩");
+                counter++;
+            } else {
+                clearInterval(intervalId);
+            }
+        }, 1000); // Alerts every 0.5 seconds
+    }, startDelay);
 };
